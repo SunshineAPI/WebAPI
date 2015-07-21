f7f5cb0a097161874a51537d56b789ca9157881b
Gil Rosenthal <gil.a.rosenthal@gmail.com>
2015-07-21 20:00:23 +0000

    Added replying and quoting, closes #7

--- routes/forums.js
+++ routes/forums.js
@@ -240,4 +240,103 @@ router.post("/topics/:section", function(req, res) {
 });
 
 
+router.post("/replies/:section", function(req, res) {
+    // move to middleware
+    var login = req.headers.authorization;
+    var key = req.params.section;
+    var arr;
+    if (login) {
+        arr = login.split(":");
+    }
+    if (!login || arr.length !== 2) {
+        return res.status(401).json({
+            errors: ["Provide login credentials"]
+        });
+    }
+
+    var pass = arr[1];
+    var username = arr[0];
+
+
+
+   var options = {
+        method: "POST",
+        url: "https://oc.tc/forums/topics/" + key + "/posts",
+        headers: {
+            "content-type": "x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
+        },
+        formData: {
+            "utf8": "✓",
+            "post[text]": req.body.contents,
+            "post[converted]": "true",
+            "commit": "Reply"
+        }
+    };
+
+    auth.authed_req(options, username, pass, function(error, response, body) {
+        if (error) {
+            return res.status(error.status).json({
+                errors: [error.message]
+            });
+        }
+        var $ = cheerio.load(body);
+        var erro = $("body > div > div.flash > div").text();
+        if (!erro) {
+            res.send("Success!");
+        } else {
+            res.status(500).send("Failed,try again soon");
+        }
+    });
+});
+router.post("/quotes/:topic", function(req, res) {
+    // move to middleware
+    var login = req.headers.authorization;
+    var key = req.params.topic;
+    var arr;
+    if (login) {
+        arr = login.split(":");
+    }
+    if (!login || arr.length !== 2) {
+        return res.status(401).json({
+            errors: ["Provide login credentials"]
+        });
+    }
+
+    var pass = arr[1];
+    var username = arr[0];
+
+
+
+   var options = {
+        method: "POST",
+        url: "https://oc.tc/forums/topics/" + key + "/posts",
+        headers: {
+            "content-type": "x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
+        },
+        formData: {
+            "utf8": "✓",
+            "post[text]": req.body.contents,
+            "post[converted]": "true",
+            "post[reply_to_id]": req.body.parentpost,
+            "commit": "Reply"
+        }
+    };
+
+    auth.authed_req(options, username, pass, function(error, response, body) {
+        if (error) {
+            return res.status(error.status).json({
+                errors: [error.message]
+            });
+        }
+        var $ = cheerio.load(body);
+        var erro = $("body > div > div.flash > div").text();
+        if (!erro) {
+            res.send("Success!");
+        } else {
+            res.status(500).send("Failed,try again soon");
+        }
+    });
+});
 module.exports = router;
