04de09e00c23b1cf8c138cb93367adcab7cd332d
Jake <jake0oo0dev@gmail.com>
2015-07-22 15:38:03 -0500

    Dynamically set URL based on host

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    res.render("index");
+    res.render("index", {
+    	domain: req.protocol + "://" + req.get("host")
+    });
 });
 
 module.exports = router;
