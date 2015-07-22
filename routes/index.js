bb278e2c4e1a955430c039b190f4faba5eace77e
Jake <jake0oo0dev@gmail.com>
2015-07-22 15:57:51 -0500

    Debug https more

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-	console.log(req.secure, "SECURE", req.protocol);
+	console.log(req.secure, "SECURE", req.protocol, req.headers['x-forwarded-proto']);
     res.render("index", {
     	domain: "http" + (req.secure ? "s" : "") + "://" + req.get("host")
     });
