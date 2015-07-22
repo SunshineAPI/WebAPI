a5c6568bb07e3ff0b87ff5c7cd0e114e54335bd9
Jake <jake0oo0dev@gmail.com>
2015-07-22 15:50:13 -0500

    Debug https

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
+	console.log(req.secure, "SECURE", req.protocol);
     res.render("index", {
     	domain: "http" + (req.secure ? "s" : "") + "://" + req.get("host")
     });
