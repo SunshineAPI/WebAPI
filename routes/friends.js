efd50f93db86b2b412aab38edba40830652fa4e7
Jake <jake0oo0dev@gmail.com>
2015-07-22 20:27:08 -0500

    convert friends - needs testing

--- routes/friends.js
+++ routes/friends.js
@@ -5,21 +5,8 @@ var parser = require("../modules/parser");
 var request = require("request");
 var cheerio = require("cheerio");
 var auth = require("../modules/auth");
-router.get("/all",function(req,res){
-    var login = req.headers.authorization;
-    var arr;
-    if (login) {
-        arr = login.split(":");
-    }
-    if (!login || arr.length !== 2) {
-        return res.status(401).json({
-            errors: ["Provide login credentials"]
-        });
-    }
 
-    var pass = arr[1];
-    var username = arr[0];
-
+router.get("/all", auth.authorize, function(req, res) {
     var options = {
         method: "GET",
         url: "https://oc.tc/friendships",
@@ -27,10 +14,10 @@ router.get("/all",function(req,res){
             "content-type": "x-www-form-urlencoded",
             "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
         }
-        
+
     };
 
-    auth.authed_req(options, username, pass, function(error, response, body) {
+    auth.authed_req(options, req.authorization.cookie, function(error, response, body) {
         if (error) {
             return res.status(error.status).json({
                 errors: [error.message]
@@ -39,28 +26,14 @@ router.get("/all",function(req,res){
         var all = new Array();
         var $ = cheerio.load(body);
         var allfriends = $(".friend-icon");
-        allfriends.each(function(i,elem){
-            all[i] = $(this).children(".thumbnail").attr("href").substring(1,$(this).children(".thumbnail").attr("href").length);
+        allfriends.each(function(i, elem) {
+            all[i] = $(this).children(".thumbnail").attr("href").substring(1, $(this).children(".thumbnail").attr("href").length);
         });
         res.json(all);
     });
 });
 
-router.get("/offline",function(req, res) {
-     var login = req.headers.authorization;
-    var arr;
-    if (login) {
-        arr = login.split(":");
-    }
-    if (!login || arr.length !== 2) {
-        return res.status(401).json({
-            errors: ["Provide login credentials"]
-        });
-    }
-
-    var pass = arr[1];
-    var username = arr[0];
-
+router.get("/offline", auth.authorize, function(req, res) {
     var options = {
         method: "GET",
         url: "https://oc.tc/friendships",
@@ -68,10 +41,10 @@ router.get("/offline",function(req, res) {
             "content-type": "x-www-form-urlencoded",
             "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
         }
-        
+
     };
 
-    auth.authed_req(options, username, pass, function(error, response, body) {
+    auth.authed_req(options, req.authorization.cookie, function(error, response, body) {
         if (error) {
             return res.status(error.status).json({
                 errors: [error.message]
@@ -80,28 +53,14 @@ router.get("/offline",function(req, res) {
         var all = new Array();
         var $ = cheerio.load(body);
         var allfriends = $(".span1");
-        allfriends.each(function(i,elem){
-            all[i] = $(this).children(".thumbnail").attr("href").substring(1,$(this).children(".thumbnail").attr("href").length);
+        allfriends.each(function(i, elem) {
+            all[i] = $(this).children(".thumbnail").attr("href").substring(1, $(this).children(".thumbnail").attr("href").length);
         });
         res.json(all);
     });
 
 });
-router.get("/online",function(req, res) {
-    var login = req.headers.authorization;
-    var arr;
-    if (login) {
-        arr = login.split(":");
-    }
-    if (!login || arr.length !== 2) {
-        return res.status(401).json({
-            errors: ["Provide login credentials"]
-        });
-    }
-
-    var pass = arr[1];
-    var username = arr[0];
-
+router.get("/online", auth.authorize, function(req, res) {
     var options = {
         method: "GET",
         url: "https://oc.tc/friendships",
@@ -109,10 +68,10 @@ router.get("/online",function(req, res) {
             "content-type": "x-www-form-urlencoded",
             "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
         }
-        
+
     };
 
-    auth.authed_req(options, username, pass, function(error, response, body) {
+    auth.authed_req(options, req.auhorization.cookie, function(error, response, body) {
         if (error) {
             return res.status(error.status).json({
                 errors: [error.message]
@@ -121,8 +80,8 @@ router.get("/online",function(req, res) {
         var all = new Array();
         var $ = cheerio.load(body);
         var allfriends = $(".span2");
-        allfriends.each(function(i,elem){
-            all[i] = $(this).children(".thumbnail").attr("href").substring(1,$(this).children(".thumbnail").attr("href").length);
+        allfriends.each(function(i, elem) {
+            all[i] = $(this).children(".thumbnail").attr("href").substring(1, $(this).children(".thumbnail").attr("href").length);
         });
         res.json(all);
     });
