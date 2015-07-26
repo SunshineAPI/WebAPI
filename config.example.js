79b22c15ad3f7e2144150c9ef7f39fc30d982185
Jake <jake0oo0dev@gmail.com>
2015-07-25 22:13:54 -0500

    Comment default config

--- config.example.js
+++ config.example.js
@@ -1,8 +1,9 @@
+"use strict";
 var config = {
-	user_agent: "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36",
-	base_url: "https://oc.tc",
-	test_url: "http://sunshineapi.github.io/sunshine-mock.github.io",
-	http_timeout: 3000
+	user_agent: "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36",    // Specify the user agent to use
+	base_url: "https://oc.tc",    // Specify the base URL for API calls
+	test_url: "http://sunshineapi.github.io/sunshine-mock.github.io",      // Specify the URL to test against, should remain the same, and should never be Overcast directly
+	http_timeout: 3000   // Specify the HTTP timeout for API requests
 };
 
 module.exports = config;
