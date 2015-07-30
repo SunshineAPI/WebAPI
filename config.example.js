37440f032b313db4864930b77c691c732c32529f
Jake <jake0oo0dev@gmail.com>
2015-07-29 21:20:07 -0500

    Set cache times

--- config.example.js
+++ config.example.js
@@ -6,6 +6,28 @@ var config = {
 	http_timeout: 3000,   // Specify the HTTP timeout for API requests
 	cache_times: {      // Specify how long each module key should be cached for in seconds
 		player: 30,
+		channels: 60 * 5,
+		forums_new: 20,
+		categories: 60 * 60 * 2,
+		category: 20,
+		topic: 20,
+		post: 30,
+		maps_playing: 45,
+		maps_all: 60 * 30,
+		map: 60 * 30,
+		gamemode: 60 * 60,
+		match: 60,
+		rotations: 60 * 30,
+		rotation: 60 * 15,
+		region: 60 * 30,
+		punishments: 60,
+		punishment: 60 * 5,
+		staff: 60 * 60,
+		stats: 60 * 15,
+		teams: 60 * 60,
+		team: 60 * 60,
+		tournaments: 60 * 60 * 6,
+		tournament: 60 * 30
 	}
 };
 
