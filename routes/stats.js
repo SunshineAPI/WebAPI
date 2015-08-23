91ccd11d08e6a726854db43a5205e94a5fbf4c15
Gil Rosenthal <gil.a.rosenthal@gmail.com>
2015-08-23 17:47:16 +0000

    Fixed leaderboard parsing

--- routes/stats.js
+++ routes/stats.js
@@ -7,32 +7,12 @@ var querystring = require("querystring");
 var helpers = require("../modules/helpers");
 var cache = require("../modules/cache");
 
-var periods = ["day", "week", "eternity"];
-var games = ["all", "projectares", "blitz", "ghostsquadron"];
-var sorts = ["kills", "deaths", "kd", "kk", "cores_leaked", "wool_placed", "destroyables_destroyed", "playing_time"];
 
 router.get("/", function(req, res) {
     var page = parseInt(req.query.page) || 1;
-    var sort = req.query.sort || "kills";
-    var game = req.query.game || "all";
-    var time = req.query.time || "day";
-
-    if (sorts.indexOf(sort) === -1 || games.indexOf(game) === -1 || periods.indexOf(time) === -1) {
-        return res.status(422).json({
-            errors: ["Invalid sort options"]
-        });
-    }
-
-    var query = querystring.stringify({
-        page: page,
-        time: time,
-        game: game,
-        sort: sort
-    });
-
     var options = {
         method: "GET",
-        url: "/stats?" + query,
+        url: "/leaderboard?page="+page,
     };
 
     helpers.request(options, function(error, response, body) {
@@ -42,7 +22,6 @@ router.get("/", function(req, res) {
                 errors: ["Unable to complete request"]
             });
         }
-
         var $ = cheerio.load(body);
         var pagination = $(".span12 .btn-group.pull-left").first();
         var pages = parser.pageCount($, pagination);
@@ -54,53 +33,29 @@ router.get("/", function(req, res) {
         var players = [];
         var links = parser.setMeta(req, page, pages);
 
-        var meta = {};
-        meta.time = time;
-        meta.game = game;
-        meta.sort = sort;
+    
 
-        var rows = $("table tbody tr");
-
-        for (var i = 0; i < rows.length; i++) {
-            var elm = $(rows[i]);
-
-            var cols = elm.children();
-
-            var place = parseInt($(cols[0]).text());
-            var kills = parseInt($(cols[1]).text());
-            var deaths = parseInt($(cols[3]).text());
-            var kd = parseFloat($(cols[3]).text());
-            var kk = parseFloat($(cols[4]).text());
-            var wool_placed = parseInt($(cols[5]).text());
-            var cores_leaked = parseInt($(cols[6]).text());
-            var destroyables_destroyed = parseInt($(cols[7]).text());
-            var playing = $(cols[8]).text().escapeSpecialChars();
-
-            var player = $(cols[9]).text().escapeSpecialChars();
-
-            players.push({
-                player: player,
-                place: place,
-                playing_time: playing,
-                stats: {
-                    kills: kills,
-                    deaths: deaths,
-                    kd: kd,
-                    kk: kk,
-                    wools: wool_placed,
-                    cores: cores_leaked,
-                    monuments: destroyables_destroyed
-                }
-            });
-        }
+        var rows = $("body > div > section > div.row > div > table > tbody > tr");
+        rows.each(function(i){
+            var newPlayer = {};
+            newPlayer.name = $(this).find("td:nth-child(8)").text().escapeSpecialChars();
+            newPlayer.rank = parseInt($(this).find("td:nth-child(1)").text());
+            newPlayer.rating = parseFloat($(this).find("td:nth-child(2)").children("strong").text());
+            newPlayer.matches = parseInt($(this).find("td:nth-child(3)").text());
+            newPlayer.winloss = parseFloat($(this).find("td:nth-child(4)").text());
+            newPlayer.wins = parseInt($(this).find("td:nth-child(5)").text());
+            newPlayer.losses = parseInt($(this).find("td:nth-child(6)").text());
+            newPlayer.forfeits = parseInt($(this).find("td:nth-child(7)").text());
+            players.push(newPlayer);
+        });
+        
         var statsResponse = {
             links: links,
-            meta: meta,
             data: players
         };
 
         res.json(statsResponse);
-        cache.cache_response(res, statsResponse, "stats");
+        //cache.cache_response(res, statsResponse, "stats");
     });
 });
 
